08b14887e094be8c5fd1d68c361cd822cf42efda
miroslavwv <miroslavv@@gmail.com>
2016-11-29 21:59:45 +0200

    dafad
    sdsda

--- src/main/java/hospitalweb/controllers/LoginController.java
+++ src/main/java/hospitalweb/controllers/LoginController.java
@@ -1,17 +1,64 @@
 package hospitalweb.controllers;
 
+import hospitalweb.Services.DoctorService;
+import hospitalweb.Services.PacientService;
+import hospitalweb.model.Doctor;
+import hospitalweb.model.Pacient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class LoginController {
 
+    @Autowired
+    PacientService pacientService;
+
+    @Autowired
+    DoctorService doctorService;
+
     @RequestMapping("/login")
-    public String login(){
+    public String login(Model model){
 
         return "login";
     }
-    //boolean checkLogin(String nickName, String password){
 
+
+    @RequestMapping("/tryLogin")
+    public String tryLog(@ModelAttribute("nickName") String nickName,@ModelAttribute("password") String password){
+
+        if(checkLogin(nickName,password)==0){
+            return "noUserExist";
+        }
+        else if(checkLogin(nickName,password)==1){
+            return "logged";
+        }
+        else if(checkLogin(nickName,password)==2){
+            return "logged";
+        }
+        return "noUserExist";
+    }
+    Integer checkLogin(String nickName, String password) {
+        List<Pacient> pc=pacientService.getAllPacients();
+        List<Doctor> dc = doctorService.getAllDoctors();
+
+        for (Pacient pacient:pc) {
+            if(pacient.getNickName().equals(nickName)&&pacient.getPassword().equals(password)){
+                return 1;
+            }
+        }
+        for(Doctor doc: dc){
+            if(doc.getNickName().equals(nickName)&&doc.getPassword().equals(password)){
+                return 2;
+            }
+        }
+
+
+    return 0;
+    }
 
 }
